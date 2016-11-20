f7a9448d7543378e0f635004b6081f4c3e22ea02
ShannenDowling <shannen.dowling@students.ittralee.ie>
2016-11-20 22:12:03 +0000

    Add files via upload

--- BankAcc.java
+++ BankAcc.java
@@ -8,11 +8,16 @@ import java.awt.*;
 public class BankAcc extends JFrame implements ActionListener{
 	
 	JButton registerButton, loginButton;
+	JMenu fileMenu, accountsMenu, optionsMenu;
+	JLabel response;
 	
 	public static void main(String args[]){
 		
 		BankAcc acc1 = new BankAcc();
 		acc1.setVisible(true);
+		
+		BankAcc menu = new BankAcc();
+		menu.setVisible(true);
 		
 		BankAcc registerButton = new BankAcc();
 		BankAcc loginButton = new BankAcc();
@@ -25,7 +30,7 @@ public class BankAcc extends JFrame implements ActionListener{
 		super("Bank Account");
 		
 		setSize(400,500);
-		setLocation(100,300);
+		setLocation(200,200);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		addWindowListener(new WindowEventHandler());
@@ -33,6 +38,23 @@ public class BankAcc extends JFrame implements ActionListener{
 		Container cPane = getContentPane();
 		cPane.setLayout(new FlowLayout());
 		cPane.setBackground(Color.lightGray);
+		
+		//menu
+		createFileMenu();
+		createAccountsMenu();
+		createOptionsMenu();
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		menuBar.setBackground(Color.orange);
+		menuBar.add(fileMenu);
+		menuBar.add(accountsMenu);
+		menuBar.add(optionsMenu);
+		
+		//response
+		response = new JLabel("Menu tester");
+		//respone.setSize(250,50);
+		cPane.add(response);
 		
 		//label
 		JLabel welcomeMsg = new JLabel("Welcome to your Bank Account.");
@@ -59,9 +81,79 @@ public class BankAcc extends JFrame implements ActionListener{
 			System.exit(0);
 		}
 	}
-	//buttons
+	
+	//fileMenu
+	private void createFileMenu(){
+		
+		JMenuItem item;
+		
+		fileMenu = new JMenu("File");
+		
+		item = new JMenuItem("Login");
+		item.addActionListener(this);
+		
+		fileMenu.add(item);
+		
+		item = new JMenuItem("Check Balance");
+		item.addActionListener(this);
+		
+		fileMenu.add(item);
+		
+		item = new JMenuItem("Quit");
+		item.addActionListener(this);
+		
+		fileMenu.add(item);
+	}
+	
+	//accountsMenu
+	private void createAccountsMenu(){
+		
+		JMenuItem item;
+		
+		accountsMenu = new JMenu("Accounts");
+		
+		item = new JMenuItem("Add");
+		item.addActionListener(this);
+		
+		accountsMenu.add(item);
+		
+		item = new JMenuItem("Remove");
+		item.addActionListener(this);
+		
+		accountsMenu.add(item);
+		
+		item = new JMenuItem("Update");
+		item.addActionListener(this);
+		
+		accountsMenu.add(item);
+	}
+	
+	//optionsMenu
+	private void createOptionsMenu(){
+		
+		JMenuItem item;
+		
+		optionsMenu = new JMenu("Options");
+		
+		item = new JMenuItem("Withdraw");
+		item.addActionListener(this);
+		
+		optionsMenu.add(item);
+		
+		item = new JMenuItem("Lodge");
+		item.addActionListener(this);
+		
+		optionsMenu.add(item);
+		
+		item = new JMenuItem("Interest");
+		item.addActionListener(this);
+		
+		optionsMenu.add(item);
+	}
+	
+	
 	public void actionPerformed(ActionEvent e){
-			
+		//buttons	
 		if(e.getSource() == registerButton)	
 		{
 			JOptionPane.showInputDialog(null,"Please enter your name");
@@ -69,11 +161,28 @@ public class BankAcc extends JFrame implements ActionListener{
 			JOptionPane.showInputDialog(null,"Please enter your email");
 		}
 			
-		else
+		else if(e.getSource() == loginButton)
 		{
 			JOptionPane.showInputDialog(null,"Please enter your email");
 			
 			JOptionPane.showInputDialog(null,"Please enter your password");	
+		}
+		//menu
+		else
+		{
+			String menuName;
+		
+			menuName = e.getActionCommand();
+		
+			if(menuName.equals("Quit"))
+			{
+				System.exit(0);
+			}
+		
+			else 
+			{
+				response.setText("Menu Item '" + menuName + "' is selected");
+			}	
 		}
 	}
 }
